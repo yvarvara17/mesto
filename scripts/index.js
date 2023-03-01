42df30ebd1d52e48ec1af1d9a8255315f2e1980d
Varvara Yudina <yvarvara@yandex.ru>
2023-03-01 09:19:49 +0300

    third attempt

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
-let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__form-input_type_name");
-let jobInput = document.querySelector(".popup__form-input_type_job");
-let title = document.querySelector(".profile__title");
-let subtitle = document.querySelector(".profile__subtitle");
-let modal = document.querySelector(".popup");
-let editButton = document.querySelector(".profile__edit-button");
-let closeButton = document.querySelector(".popup__close");
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__form-input_type_name');
+let jobInput = document.querySelector('.popup__form-input_type_job');
+let title = document.querySelector('.profile__title');
+let subtitle = document.querySelector('.profile__subtitle');
+let modal = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close');
 function openModal(){
-  modal.classList.add("popup_opened");
+  modal.classList.add('popup_opened');
   nameInput.value = title.textContent;
   jobInput.value = subtitle.textContent;
 }
 function closeModal(){
-  modal.classList.remove("popup_opened");
+  modal.classList.remove('popup_opened');
 }
 function handleFormSubmit (evt) {
   evt.preventDefault();
@@ -21,5 +21,5 @@ function handleFormSubmit (evt) {
   closeModal();
 }
 formElement.addEventListener('submit', handleFormSubmit);
-editButton.addEventListener("click", openModal);
-closeButton.addEventListener("click", closeModal);
+editButton.addEventListener('click', openModal);
+closeButton.addEventListener('click', closeModal);
